32b1c5ec72cd00c76800583900193ddb2750341a
Daniel Farrell <daniel.farrell@argos.co.uk>
2020-11-29 11:38:59 +0000

    jest, react testing library

---
+++ src/components/Header/Header.spec.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Header from "./Header";
+
+it("test jest setup", () => {
+  expect(0.1 + 0.2).toEqual(0.30000000000000004);
+});
+
+it("should display the company name", () => {
+  render(<Header />);
+  expect(screen.getByText("Argoose"));
+});
